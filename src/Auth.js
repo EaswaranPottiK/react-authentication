652f997f0ab7d2093d635cde9b547934cb9e5075
Easwaran <easwaranpottiak@gmail.com>
2024-01-28 22:50:37 +0530

    commit

--- src/Auth.js
+++ src/Auth.js
@@ -32,17 +32,8 @@ export const Auth = (props) => {
         // The signed-in user info.
         const user = result.user;
         props.setAccessTocken(token)
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
       }).catch((error) => {
-        // Handle Errors here.
         const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
       });
   };
 
