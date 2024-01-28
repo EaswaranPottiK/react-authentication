7d1e1824a8483110ed191713bbd05f38c0f6be5b
Easwaran <easwaranpottiak@gmail.com>
2024-01-28 22:54:52 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<App/>
-  }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={router} />
+  <App />
 );
 
 reportWebVitals();
